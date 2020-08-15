e9437acd518eafc84ef63aadaea59018288db568
Donzasto <fordealing@yandex.ru>
2020-08-15 13:02:52 +0300

    s14 css-modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import s from './Profile.module.css';
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'></img>
             </div>
             <div>
-                <img src='https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg'/>
+                <img src='https://www.meme-arsenal.com/memes/4755e2d723dc73f9757832eff0992707.jpg'/>
             </div>
             <div>
                 ava + description
@@ -17,11 +17,11 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className ={s.posts}>
+                    <div className ={s.item}>
                         post 1
                     </div>
-                    <div>
+                    <div className ={s.item}>
                         post 2
                     </div>
                 </div>
