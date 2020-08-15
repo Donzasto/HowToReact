cfb8e6c32858c3823333df99eea89ce217b814d2
Donzasto <fordealing@yandex.ru>
2020-08-15 14:24:40 +0300

    Fix repo

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img
+                    src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'></img>
+            </div>
+            <div>
+                <img src='https://www.meme-arsenal.com/memes/4755e2d723dc73f9757832eff0992707.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts/>
+        </div>)
+}
+
+export default Profile;
