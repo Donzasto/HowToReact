051d3886149961eb5bd09606c082c02e06f2ca5b
Donzasto <fordealing@yandex.ru>
2020-08-16 17:38:48 +0300

    s21 add css for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,17 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) =>{
     return(
-        <div className={s.color}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sveta
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>"How is your"</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import s  from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={`${s.item} ${s.active}`}>
-                <a href="/profile">Profile</a>
+            <div className={`${s.item}`}>
+                <NavLink to="/profile" activeClassName={s.activeLnk}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" activeClassName={s.activeLnk}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news" activeClassName={s.activeLnk}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music" activeClassName={s.activeLnk}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings" activeClassName={s.activeLnk}>Settings</NavLink>
             </div>
         </nav>)
 }
