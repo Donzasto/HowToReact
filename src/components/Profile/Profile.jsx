8cf7e098fe30de78107cbfd0b5995b75ec7445c0
Donzasto <fordealing@yandex.ru>
2020-08-22 08:54:41 +0300

    s31 callback through props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = (props) => {
     return (
         <div>
             <ProfileInfo></ProfileInfo>
-            <MyPosts posts={props.state.posts}/>
+            <MyPosts posts={props.state.posts} addPost={props.addPost}/>
         </div>)
 }
 
