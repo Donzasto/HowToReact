81401fa6b578f0363c52c1246bc0a55f8f59c78d
Donzasto <fordealing@yandex.ru>
2020-08-25 20:40:05 +0300

    s40 add message

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = (props) => {
             <div className='app-wrapper-content'>
                 <Route path='/profile'
                        render={() => <Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />}/>
-                <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+                <Route path='/dialogs' render={() => <Dialogs store={props.store} />}/>
                 <Route path='/news' component={News}/>
                 <Route path='/music' component={Music}/>
                 <Route path='/settings' component={Settings}/>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,7 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
+const SEND_MESSAGE = "SEND-MESSAGE";
 
 let store = {
     _state: {
@@ -13,7 +15,8 @@ let store = {
                 {id: 1, message: "Hi"},
                 {id: 2, message: "How is your"},
                 {id: 3, message: "Yo"}
-            ]
+            ],
+            newMessageBody:""
         },      
         profilePage:{
             posts: [
@@ -52,6 +55,14 @@ let store = {
         }else if(action.type === UPDATE_NEW_POST_TEXT){
             this._state.profilePage.newPostText = action.newText;
             this._callSubcsriber(this._state);
+        }else if(action.type === UPDATE_NEW_MESSAGE_BODY){
+            this._state.dialogsPage.newMessageBody = action.body;
+            this._callSubcsriber(this._state);
+        }else if(action.type === SEND_MESSAGE){
+            let body = this._state.dialogsPage.newMessageBody;
+            this._state.dialogsPage.newPostText = "";
+            this._state.dialogsPage.messages.push({id: 6, message: body});
+            this._callSubcsriber(this._state);
         }
     }
 }
@@ -59,5 +70,9 @@ let store = {
 export const addPostActionCreator = () => ({type: ADD_POST});
 
 export const updateNewPostTextActionCreator = (text) =>({type: UPDATE_NEW_POST_TEXT, newText: text});
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE});
+
+export const updateNewMessageBodyCreator = (body) =>({type: UPDATE_NEW_MESSAGE_BODY, body: body});
 
 export default store
