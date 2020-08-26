39f8048e7cf5f586904a286fb7c9fd7421d9a9c5
Donzasto <fordealing@yandex.ru>
2020-08-26 12:33:07 +0300

    s41 reducer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,78 +1,55 @@
-const ADD_POST = "ADD-POST";
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
-const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
-const SEND_MESSAGE = "SEND-MESSAGE";
+import profileReducer from "./profileReducer";
+import dialogsReducer from "./dialogsReducer";
+import sidebarReducer from "./sidebarReducer";
 
 let store = {
     _state: {
-        dialogsPage:{
+        dialogsPage: {
             dialogs: [
-                {id: 1, name: "Sveta"},
-                {id: 2, name: "Andrey"},
-                {id: 3, name: "Vasiliy"}
-            ],      
+                { id: 1, name: "Sveta" },
+                { id: 2, name: "Andrey" },
+                { id: 3, name: "Vasiliy" }
+            ],
             messages: [
-                {id: 1, message: "Hi"},
-                {id: 2, message: "How is your"},
-                {id: 3, message: "Yo"}
+                { id: 1, message: "Hi" },
+                { id: 2, message: "How is your" },
+                { id: 3, message: "Yo" }
             ],
-            newMessageBody:""
-        },      
-        profilePage:{
+            newMessageBody: ""
+        },
+        profilePage: {
             posts: [
-                {id: 1, message: "Hi? how are you?", likesCount: 15},
-                {id: 2, message: "It's my firs post", likesCount: 27}
+                { id: 1, message: "Hi? how are you?", likesCount: 15 },
+                { id: 2, message: "It's my firs post", likesCount: 27 }
             ],
-            newPostText:"it"
-        },  
-        navbar:{
-            friends:[
-                {id:1, name:"Sveta"},
-                {id:2, name: "Lena"},
-                {id:3, name: "Sanya"}
-            ]   
+            newPostText: "it"
+        },
+        navbar: {
+            friends: [
+                { id: 1, name: "Sveta" },
+                { id: 2, name: "Lena" },
+                { id: 3, name: "Sanya" }
+            ]
         }
     },
-    _callSubcsriber(){
+    _callSubcsriber() {
         console.log("State changed")
     },
-    subscribe (observer) {
+    subscribe(observer) {
         this._callSubcsriber = observer;
     },
-    getState(){
+    getState() {
         return this._state;
     },
-    dispatch(action){
-        if(action.type === ADD_POST){
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            }
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText = "";
-            this._callSubcsriber(this._state);
-        }else if(action.type === UPDATE_NEW_POST_TEXT){
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubcsriber(this._state);
-        }else if(action.type === UPDATE_NEW_MESSAGE_BODY){
-            this._state.dialogsPage.newMessageBody = action.body;
-            this._callSubcsriber(this._state);
-        }else if(action.type === SEND_MESSAGE){
-            let body = this._state.dialogsPage.newMessageBody;
-            this._state.dialogsPage.newPostText = "";
-            this._state.dialogsPage.messages.push({id: 6, message: body});
-            this._callSubcsriber(this._state);
-        }
+    dispatch(action) {
+
+        profileReducer(this._state.profilePage, action);
+        sidebarReducer(this._state.navbar, action);
+        dialogsReducer(this._state.dialogsPage, action);
+
+        this._callSubcsriber(this._state);
+
     }
 }
-
-export const addPostActionCreator = () => ({type: ADD_POST});
-
-export const updateNewPostTextActionCreator = (text) =>({type: UPDATE_NEW_POST_TEXT, newText: text});
-
-export const sendMessageCreator = () => ({type: SEND_MESSAGE});
-
-export const updateNewMessageBodyCreator = (body) =>({type: UPDATE_NEW_MESSAGE_BODY, body: body});
 
 export default store
