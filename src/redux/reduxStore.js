f0b2027f4fb389827caa29dc984880ceb8a7ed5b
Donzasto <fordealing@yandex.ru>
2020-09-15 19:32:22 +0300

    s61 cookie, login in theory, auth/me

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -2,13 +2,15 @@ import profileReducer from "./profileReducer"
 import navbarReducer from "./navbarReducer"
 import dialogsReducer from "./dialogsReducer"
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducer";
 const { createStore, combineReducers } = require("redux");
 
 let reducers = combineReducers({
     profilePage: profileReducer, 
     navbarPage: navbarReducer,
     dialogsPage: dialogsReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);
