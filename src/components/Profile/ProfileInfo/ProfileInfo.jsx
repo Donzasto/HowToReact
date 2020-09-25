f79733846231224cd3de67928a22a4ab59428fc0
Donzasto <fordealing@yandex.ru>
2020-09-25 18:51:43 +0300

    s71 setState, local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if(!props.profile){
@@ -8,16 +9,16 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
+            {/*<div>
                 <img
                     src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'></img>
-            </div>
+            </div>*/}
             <div>
                 <img src='https://www.meme-arsenal.com/memes/4755e2d723dc73f9757832eff0992707.jpg'/>
             </div>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava + description
+                <ProfileStatus status={"Hello my friends"}/>
             </div>
 
         </div>)
