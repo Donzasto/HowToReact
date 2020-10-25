655956c5dbb90edd0ea69330d5173e003f0b6188
Donzasto <fordealing@yandex.ru>
2020-10-25 09:53:50 +0300

    s91 chrome extension for react

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -3,7 +3,7 @@ import navbarReducer from "./navbarReducer"
 import dialogsReducer from "./dialogsReducer"
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
-import {applyMiddleware} from "redux";
+import {applyMiddleware, compose} from "redux";
 import thunkMiddleware from "redux-thunk"
 import {reducer as formReducer} from 'redux-form'
 import {appReducer} from "./appReducer";
@@ -20,6 +20,7 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store;
